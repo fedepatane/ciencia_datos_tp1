62d6e9c89e8e8b2329df730854ab46a8762440df
fede <fedepatane20@gmail.com>
2017-08-16 23:32:29 -0300

    subo tests

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,23 +1,42 @@
 import scipy.stats as sp
 
-def test_muestras_apareadas(data):
+def test_muestras_apareadas(d1, d2):
 	"""
-	En este test vamos a probar si las distribuciones de dias soleados
-	y dias nublados tienen la misma media. Para eso hay que usar el test de muestras
+	En este test vamos a probar si las distribuciones d1
+	y d2 tienen la misma media. Para eso hay que usar el test de muestras
 	apareadas. Esta es la funcion para utilizar scipy.stats.ttest_rel
 
-	Hipotesis nula : Las distribuciones de tiempos en dias soleados y dias nublados
-	tienen la misma media.
+	Hipotesis nula : Las distribuciones d1 y d2 tienen la misma media.
 	
 	"""
 
-	muestra_dias_soleados = data[:,0]
-	muestra_dias_nublados = data[:,1]
-	f = sp.ttest_rel(muestra_dias_soleados,muestra_dias_nublados)
+	f = sp.ttest_rel(d1,d2)
+	return f[0] , f[1] 
+
+def test_muestras_independientes(d1, d2):
+	"""
+	En este test vamos a probar si las distribuciones d1
+	y d2 son independientes. Esta es la funcion para utilizar scipy.stats.ttest_rel
+
+	Hipotesis nula : Las distribuciones d1 y d2 son independientes.
+	
+	"""
+
+	f = sp.ttest_ind(d1,d2)
 	return f[0] , f[1] 
 
 
+
 def ejercicio2(data):
+	muestras_dias_soleados = data[:,0]
+	muestras_dias_nublados = data[:,1]
+	muestras_dias_lluviosos = data[:,2]
+
 	# aca hay q hacer tests nuestros y probar hipotesis 
-	estadistico , pvalor = test_muestras_apareadas(data)
+	estadistico , pvalor = test_muestras_apareadas(muestras_dias_soleados, muestras_dias_nublados)
+	print estadistico , pvalor
+	estadistico , pvalor = test_muestras_apareadas(muestras_dias_soleados, muestras_dias_lluviosos)
+	print estadistico , pvalor
+	estadistico , pvalor = test_muestras_independientes(muestras_dias_soleados, muestras_dias_lluviosos)
+	print estadistico , pvalor
 
