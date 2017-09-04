c652a8591c2e524f17e5b00e86409f568530056e
fede <fedepatane20@gmail.com>
2017-09-04 02:31:25 -0300

    el ultimo codigo de los tests

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,5 +1,7 @@
 import scipy.stats as sp
 import numpy as np 
+import operator 
+#from funciones import *
 
 def test1(d1, d2):
 
@@ -12,48 +14,51 @@ def test1(d1, d2):
 	H1 = La distribuion de d2 tiene media mayor que la distribucion d1
 	Vamos a usar el test de scipy.stats.mannwhitneyu
 	"""
-	f = sp.mannwhitneyu(d1, d2, alternative='greater')
+	np.random.shuffle(d1)
+	np.random.shuffle(d2)
+	f = sp.mannwhitneyu(d1, d2, alternative='less')
 	return f[0] , f[1] 
 
-def test2(d1, d2):
+def test3(d1, d2, d3):
 	"""
-	ni idea hay que preguntarlo , pero me parece que hay que buscar que tengan
-	la misma media y ver que no la tienen ?? 
+		H0 = Las distribuciones son independientes. 
 	"""
-
-	return 1 , 1
-def test3(d1, d2):
-	"""
-		H0 = La velocidad en dias de lluvia es independiente del atleta
-		Esto significa que por ejemplo, las variables de lluvia y la de nublado son 
-		independientes. Test de dos colas por su hipotesis alternativa
-		Test a usar : ttest_ind
-	"""
-	f = sp.ttest_ind(d1,d2)
-	return f[0] , f[1]
-
-
-
-
-def test4(d1, d2):
-
-	"""
-	Premisa = El tiempo influye en la velocidad de los atletas.
-	Osea queremos ver que d1 tiene una media distinta que d2. 
-	Para eso tenemos que usar un test de dos colas
-	H0 = Las muestras provienen de distribuciones de igual media
-	H1 = La distribuion de d2 tiene media distinta que la distribucion d1
-	Vamos a usar el test de muestras apareadas. 
-	ttest_rel
-	"""
-	f = sp.ttest_rel(d1, d2)
-	return f[0] , f[1] 
+	soleado = {}
+	nublado = {}
+	lluvioso = {}
+	i = 1
+	for t in d1 : 
+		soleado[i] = t
+		i = i + 1
+	i = 1
+	for t in d2 : 
+		nublado[i] = t
+		i = i + 1	
+	i = 1
+	for t in d3 : 
+		lluvioso[i] = t
+		i = i + 1
 
 
 
+	sorted_soleado = sorted(soleado.items(), key=operator.itemgetter(1))
+	sorted_nublado = sorted(nublado.items(), key=operator.itemgetter(1))
+	sorted_lluvioso = sorted(lluvioso.items(), key=operator.itemgetter(1))
+	i = 0 
+	while i <= len(sorted_lluvioso) - 1 : 
+		print str(sorted_soleado[i][0]) + " , " + str(sorted_nublado[i][0]) + " , " + str(sorted_lluvioso[i][0])
+		i = i + 1
+	print "------------------------------------------------------"
+	return 1 , 1 
+	#return f[0] , f[1]
+
+
+def test4(d1, d2, d3):
+	#HACERRRRRRRRRRR
+	return 1 , 1 
 def ejercicio3(data):
 	
-	soleado = data[:, 0] 
+	soleado = data[:, 0]
 	nublado = data[:, 1]
 	lluvioso = data[:, 2] 
 	media1 = np.mean(soleado)
@@ -61,16 +66,17 @@ def ejercicio3(data):
 	media3 =  np.mean(lluvioso)
 	# media1 y media2 son iguales 
 
-	estadistico , pvalor = test1(soleado, lluvioso)
+	#estadistico , pvalor = test1(soleado, lluvioso)
+	#print estadistico , '%.10f' % pvalor
+
+	#estadistico , pvalor = test2(nublado, soleado)
+	#print estadistico , '%.10f' % pvalor
+
+	estadistico , pvalor = test3(soleado, nublado, lluvioso)
 	print estadistico , '%.10f' % pvalor
+	
+	#test4(soleado, nublado, lluvioso)
+	#print estadistico , '%.10f' % pvalor
 
-	estadistico , pvalor = test2(soleado, nublado)
-	print estadistico , '%.10f' % pvalor
-
-
-	estadistico , pvalor = test3(soleado, lluvioso)
-	print estadistico , '%.10f' % pvalor
-
-
-	estadistico , pvalor = test4(nublado, lluvioso)
-	print estadistico , '%.10f' % pvalor
+	#estadistico , pvalor = test5(lluvioso, soleado)
+	#print estadistico , '%.10f' % pvalor
--- tp1.py
+++ tp1.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pylab as plt
 import pandas as pd 
 import seaborn as sns
-from ejercicio2 import *
 from ejercicio3 import * 
 import scipy.stats as ss
 
@@ -32,24 +31,26 @@ def plot(data, data_type):
 	plt.xlabel("Segundos que se tarda en hacer los 100 mts.")
 	plt.ylabel("Cantidad de atletas que lo logran en ese tiempo.")
 	plt.title("Distribucion de los tiempos de corrida, por estado del dia. Datos " + data_type)
-	#plt.show()
+	plt.savefig("hist_"+data_type+".png")
+	plt.show()
 
 	plt.subplot(1,3,1)
 	plt.plot(d1, "r-" , marker='o')
-	plt.title("Tiempo Soleado. Datos " + data_type)
+	plt.title("Soleado,datos " + data_type+".")
 	plt.xlabel("Numero atleta")
 	plt.ylabel("Tiempo en segundos para los 100 mts")
 	plt.subplot(1,3,2)
 	plt.plot(d2, "b-", marker='o')
-	plt.title("Tiempo Nublado. Datos " + data_type)
+	plt.title("Nublado,datos " + data_type+".")
 	plt.xlabel("Numero atleta")
 	plt.ylabel("Tiempo en segundos para los 100 mts")
 	plt.subplot(1,3,3)
 	plt.plot(d3, "g-", marker='o')
-	plt.title("Tiempo Lluvioso. Datos " + data_type)
+	plt.title("Lluvioso,datos " + data_type+".")
 	plt.xlabel("Numero atleta")
 	plt.ylabel("Tiempo en segundos para los 100 mts")
-	#plt.show()
+	plt.savefig("plt_"+data_type+".png")
+	plt.show()
 
 	plt.plot(d1, label="Tiempo soleado")
 	plt.plot(d2, label="Tiempo nublado")
@@ -58,25 +59,24 @@ def plot(data, data_type):
 	plt.ylabel("Tiempo en segundos para los 100 mts")
 	plt.title("Estados del tiempo, superpuestos.Datos " + data_type)
 	plt.legend(loc='upper right')
-	#plt.show()
-
+	plt.savefig("plt2_"+data_type+".png")
+	plt.show()
 	
 	data_frame = pd.DataFrame(data)
 	data_frame.columns = ["soleado" , "nublado" , "lluvioso"]
 
 	sns_plot = sns.pairplot(data_frame)
-	#plt.show()
-
+	plt.savefig("pair_"+data_type+".png")
+	plt.show()
 
 
 data = np.loadtxt('tiempos.txt')
-plot(data , "sucios")
+#plot(data , "sucios")
 data = cleanData(data)
-plot(data, "limpios")
+#plot(data, "limpios")
 
 
 
-#ejercicio2(data)
 ejercicio3(data)
 
 
